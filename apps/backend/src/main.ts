39254d1c96c6a16e5bb17a1a8122c24d9750857e
Rizkyrayhan <rizkyrayhan123@gmail.com>
2026-06-08 18:33:05 +0700

    feat: register PrismaExceptionFilter as global exception handler in main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
+
+  app.useGlobalFilters(new PrismaExceptionFilter());
 
   app.enableCors();
   
